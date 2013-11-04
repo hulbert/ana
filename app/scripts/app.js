51cea673cb47ccafc7fb97e8cac568d19d9ff19d
Scott Hulbert <scott@scotthulbert.com>
2013-11-04 00:20:56 -0800

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,5 @@
+/*global define */
+define(['raphael', 'jquery'], function ($) {
+    'use strict';
+	
+});
---
+++ app/scripts/main.js
@@ -0,0 +1,10 @@
+require.config({
+    paths: {
+        jquery: '../bower_components/jquery/jquery',
+		raphael: '../bower_components/raphael/raphael'
+    }
+});
+
+require(['app', 'jquery'], function (app, $) {
+    'use strict';
+});
