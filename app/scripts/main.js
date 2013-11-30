cd1401c1a3759c9597f84e14ee892178bd082711
Scott Hulbert <scott@scotthulbert.com>
2013-11-29 20:23:11 -0800

    use requirejs shim to make magnific popup dependent on jQuery

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,12 @@ require.config({
         'jquery': '../bower_components/jquery/jquery',
 		'magnific': '../bower_components/magnific-popup/dist/jquery.magnific-popup',
 		'raphael': '../bower_components/raphael/raphael'
-    }
+    },
+	shim: {
+		'magnific': {
+            deps: ['jquery']
+        }
+	}
 });
 
 require(['app']);
